428ed0511a5c016fe9735b78ee52bbd953f427ac
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-10 15:22:39 +0100

    exo 2 functionnal but ugly like angela merkel?

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # Read_all
+  # get "tasks", to: "tasks#index"
+  # # Create
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+  # # Udpate
+  # get "tasks/:id/edit", to: "tasks#edit", as: :tasks_edit
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # Destroy
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  # # Read_find
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  resources :tasks
 end
